a6590471afad634125d98581fdf02fce43cd5da5
bharatgupta77 <guptabharat297@gmail.com>
2022-05-20 14:03:30 +0530

    updated home component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,7 +3,7 @@ import "./home.css"
 
 function Home() {
     return (
-      <h2 class="text-center">Welcome to home page....</h2>
+      <h2 class="text-center">Welcome to home page!!!</h2>
     )
 }
 
